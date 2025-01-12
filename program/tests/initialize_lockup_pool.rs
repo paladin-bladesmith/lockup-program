942ece5f62943d8965df5b475264feba7c9e638e
OliverNChalk <11343499+OliverNChalk@users.noreply.github.com>
2025-01-12 11:28:28 +1100

    add ok_initialize case

--- program/tests/initialize_lockup_pool.rs
+++ program/tests/initialize_lockup_pool.rs
@@ -3,7 +3,7 @@
 mod setup;
 
 use {
-    paladin_lockup_program::state::LockupPool,
+    paladin_lockup_program::state::{LockupPool, LockupPoolEntry},
     setup::setup,
     solana_program_test::*,
     solana_sdk::{
@@ -14,7 +14,47 @@ use {
         system_instruction,
         transaction::{Transaction, TransactionError},
     },
+    spl_discriminator::SplDiscriminate,
 };
+
+#[tokio::test]
+async fn ok_initialize() {
+    let mut context = setup().start_with_context().await;
+    let pool = Keypair::new();
+
+    // Initialize the pool.
+    let rent = Rent::default().minimum_balance(LockupPool::LEN);
+    let fund = system_instruction::transfer(&context.payer.pubkey(), &pool.pubkey(), rent);
+    let allocate = system_instruction::allocate(&pool.pubkey(), LockupPool::LEN as u64);
+    let assign = system_instruction::assign(&pool.pubkey(), &paladin_lockup_program::ID);
+    let initialize_lockup_pool =
+        paladin_lockup_program::instruction::initialize_lockup_pool(pool.pubkey());
+    let tx = Transaction::new_signed_with_payer(
+        &[fund, allocate, assign, initialize_lockup_pool],
+        Some(&context.payer.pubkey()),
+        &[&context.payer, &pool],
+        context.last_blockhash,
+    );
+    context.banks_client.process_transaction(tx).await.unwrap();
+
+    // The pool matches the expected state.
+    let pool = context
+        .banks_client
+        .get_account(pool.pubkey())
+        .await
+        .unwrap()
+        .unwrap()
+        .data;
+    let pool = bytemuck::from_bytes::<LockupPool>(&pool);
+    assert_eq!(
+        pool,
+        &LockupPool {
+            discriminator: LockupPool::SPL_DISCRIMINATOR.into(),
+            entries: [LockupPoolEntry::default(); 1024],
+            entries_len: 0,
+        }
+    );
+}
 
 #[tokio::test]
 async fn err_duplicate_initialize() {
